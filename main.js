84fad0a65e30f60b86dc57cb0d60001cc32ba2f1
domagojkuzmek <domagoj.kuzmek421@gmail.com>
2023-12-27 13:41:20 +0100

    Add item function added, tablet layout style added

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import "./scss/style.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const inputBar = document.querySelector(".inputBar");
+    const list = document.querySelector(".list");
+    const submitBtn = document.querySelector(".submit");
+    
+    function addItemList(){
+        const itemText = inputBar.value;
+        if (itemText === ""){
+            alert("Please add an item");
+        }else {
+            list.innerHTML +=  `<li class="list__item">${itemText}<i class="fa-solid fa-xmark delete" role="button"></i></li>`;
+            inputBar.value = "";
+        }
+        // Remove item 
+        
+    }
+    
+    submitBtn.addEventListener("click", addItemList);
+    
+});
